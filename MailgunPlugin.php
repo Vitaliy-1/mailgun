fc474489561820dedf433808129149fd913e92bc
Vitalii Bezsheiko <vitaliybezsh@gmail.com>
2023-03-20 22:36:03 +0200

    Use OJS config settings instead of the plugin's form

--- MailgunPlugin.php
+++ MailgunPlugin.php
@@ -16,13 +16,9 @@
 namespace APP\plugins\generic\mailgun;
 
 use APP\core\Application;
-use APP\core\Request;
-use PKP\core\JSONMessage;
-use PKP\linkAction\LinkAction;
-use PKP\linkAction\request\AjaxModal;
 use PKP\plugins\GenericPlugin;
-use Illuminate\Support\Facades\Config;
-use PKP\core\PKPApplication;
+use Illuminate\Support\Facades\Config as ContainerConfig;
+use PKP\config\Config;
 
 require_once(dirname(__FILE__) . '/vendor/autoload.php');
 
@@ -54,126 +50,23 @@ class MailgunPlugin extends GenericPlugin
 
     protected function setConfig()
     {
-        $context = Application::get()->getRequest()->getContext();
-        $contextId = $context ? $context->getId() : PKPApplication::CONTEXT_SITE;
+        $defaultMailer = Config::getVar('email', 'default');
+        if ($defaultMailer !== 'mailgun') {
+            return;
+        }
 
-        Config::set('mail.mailers.mailgun', [
-            'transport' => 'mailgun',
-            'domain' => $this->fallbackToSiteData('domain', $contextId),
-            'secret' => $this->fallbackToSiteData('secret', $contextId),
-            'endpoint' => $this->fallbackToSiteData('endpoint', $contextId),
+        ContainerConfig::set('mail.mailers.mailgun', [
+            'transport' => $defaultMailer,
+            'domain' => Config::getVar('email', 'domain'),
+            'secret' => Config::getVar('email', 'secret'),
+            'endpoint' => Config::getVar('email', 'endpoint'),
         ]);
 
-        Config::set('mail.default', 'mailgun');
+        ContainerConfig::set('mail.default', $defaultMailer);
     }
 
-    /**
-     * If context-specific data isn't set, get site-wide plugin settings
-     */
-    protected function fallbackToSiteData(string $dataSetting, int $contextId)
-    {
-        $dataValue = $this->getSetting($contextId, $dataSetting);
-        if ($contextId === PKPApplication::CONTEXT_SITE) {
-            return $dataValue;
-        }
-
-        if (empty($dataValue)) {
-            return $this->getSetting(PKPApplication::CONTEXT_SITE, $dataSetting);
-        }
-
-        return $dataValue;
-    }
-
-    /**
-     * Enable the settings form in the site-wide plugins list
-     *
-     * @return boolean
-     */
-    public function isSitePlugin(): bool
+    public function isSitePlugin()
     {
         return true;
-    }
-
-    /**
-     * Add a settings action to the plugin's entry in the
-     * plugins list.
-     *
-     * @param Request $request
-     * @param array $actionArgs
-     * @return array
-     */
-    public function getActions($request, $actionArgs)
-    {
-        // Get the existing actions
-        $actions = parent::getActions($request, $actionArgs);
-
-        // Only add the settings action when the plugin is enabled
-        if (!$this->getEnabled()) {
-            return $actions;
-        }
-
-        // Create a LinkAction that will make a request to the
-        // plugin's `manage` method with the `settings` verb.
-        $router = $request->getRouter();
-        import('lib.pkp.classes.linkAction.request.AjaxModal');
-        $linkAction = new LinkAction(
-            'settings',
-            new AjaxModal(
-                $router->url(
-                    $request,
-                    null,
-                    null,
-                    'manage',
-                    null,
-                    [
-                        'verb' => 'settings',
-                        'plugin' => $this->getName(),
-                        'category' => 'generic'
-                    ]
-                ),
-                $this->getDisplayName()
-            ),
-            __('manager.plugins.settings'),
-            null
-        );
-
-        // Add the LinkAction to the existing actions.
-        // Make it the first action to be consistent with
-        // other plugins.
-        array_unshift($actions, $linkAction);
-
-        return $actions;
-    }
-
-    /**
-     * Show and save the settings form when the settings action
-     * is clicked.
-     *
-     * @param array $args
-     * @param Request $request
-     * @return JSONMessage
-     */
-    public function manage($args, $request) {
-        switch ($request->getUserVar('verb')) {
-            case 'settings':
-
-                // Load the custom form
-                $form = new MailgunPluginSettingsForm($this);
-
-                // Fetch the form the first time it loads, before
-                // the user has tried to save it
-                if (!$request->getUserVar('save')) {
-                    $form->initData();
-                    return new JSONMessage(true, $form->fetch($request));
-                }
-
-                // Validate and save the form data
-                $form->readInputData();
-                if ($form->validate()) {
-                    $form->execute();
-                    return new JSONMessage(true);
-                }
-        }
-        return parent::manage($args, $request);
     }
 }
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ An example of OJS 3.4 + plugin to send emails via Mailgun API
 * `git clone https://github.com/Vitaliy-1/mailgun.git`
 * `cd mailgun`
 * `composer install`
+## Configuration
+The plugin expects the following new settings to be set in the [email section](https://github.com/pkp/ojs/blob/2f7231dd491408553657c48247ab4f79e2bf16e2/config.TEMPLATE.inc.php#L293-L356) 
+of the OJS config file: `domain`, `secret`, `endpoint`. Existing email setting `transport` should be set to `mailgun`.
